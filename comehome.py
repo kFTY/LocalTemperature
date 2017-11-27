212b383b20d8a66f5974cb50a96c6994537edaf9
Di Yan <kFTY@users.noreply.github.com>
2017-11-27 17:14:34 +0100

    1st release

---
+++ comehome.py
@@ -0,0 +1,6 @@
+import nmap
+nm = nmap.PortScanner()
+for host in nm.all_hosts():
+    print('----------------------------------------------------')
+    print('Host : %s (%s)' % (host, nm[host].hostname()))
+    print('State : %s' % nm[host].state())
