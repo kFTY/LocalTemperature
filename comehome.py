7b95e62e62a0f117bd65ebf6c16126355d816aff
Di Yan <kFTY@users.noreply.github.com>
2017-11-27 21:04:22 +0100

    Update comehome.py

--- comehome.py
+++ comehome.py
@@ -7,5 +7,8 @@ def CheckExist(ip_addr, port):
     return nm[ip_addr].state() == "up"
 
 
-if CheckExist("192.168.1.1", "22"):
-    print("R7000 is at home")
+if CheckExist("192.168.1.4", "22"):
+    print("YanDi is at home")
+
+if CheckExist("192.168.1.5", "22"):
+	print("Lajie is at home")
